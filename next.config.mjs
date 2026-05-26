93371ddd50ac5a28cd6b7a503c198541107acd74
Rodrigo Henriques <rodrigomatiashenriques@gmail.com>
2026-05-26 17:11:08 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
-const nextConfig = {
-  output: 'export',
-}
+import { NextConfig } from "next";
 
-export default nextConfig
-
+// Inicializa o ambiente de desenvolvimento para a Cloudflare
 import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+
+const nextConfig: NextConfig = {
+  output: 'standalone', // Garante que o Next.js gera o formato correto para o OpenNext
+};
+
+export default nextConfig;
